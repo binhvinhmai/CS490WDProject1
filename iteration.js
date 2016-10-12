05fd4ed50b7ed709dfb66e942f2f6453100d07d5
Lucy Kull <lucy.r.kull@gmail.com>
2016-10-12 18:43:47 -0500

    Update iteration.js
    ahhhhhhhhhhhhhhhhhhhh

--- iteration.js
+++ iteration.js
@@ -1,27 +1,47 @@
-$(document).ready(function () { 
-	load_images();
-});
+<!doctype html>
+<html>
+    <head>
+        <title>Movie Search System</title>
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <meta charset="utf-8" />
+        <script id="movie-template" type="text/html">
+        <div class='movie'>
+            <img src='{{path}}'>
+            <div class='description'>{{description}}</div>
+            <div class='year'>{{year}}</div>    
+        </div> 
+    </script>
+    <script src="htmlMaker.js"></script>
+    <script src="data.js"></script>
+    <script src="movies.js"></script>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body> 
+        <div id = "fixed_header">
+            <div id="header">
+                <img src="images/movie.png">
+                <h4>RENT A MOVIE</h4>
+                <div id="search_feature">
+                    <input type="text" id="search_bar" value="Search for movies">
+                    <input type="button" id="search_button" value="Search">
+                    <div id="suggestions_box" class="suggestions">
 
+                    </div>
+                </div>
+            </div>
+            <div id="icons_bar">
+                <p>Sort By:</p>
+                <select id="sort_dropdown">
+                    <option value="year">&#160;Year</option>
+                    <option value="rating">&#160;Rating</option>
+                </select>
+                <img src="images/grid.jpg" id="grid_image">
+                <img src="images/list.jpg" id="list_image">
+            </div>
+        </div>
 
-function load_images(){
-	
-var data=movies["movies"];
-var html="";
-for(var i=0;i<data.length;i++)
-	html+=make_image(data[i]);
-
-$("#photos").html(html);
-}
-
-/* this function reads one row of a JSON table, and returns html code 
- * that shows the image information (e.g. picture, description)*/
-
-function make_image(data){
-	var html="";
-	html+="<div class='photo'>";
-	html+="<img src='"+data["photo"]+"'>";
-	html+="<div class='description'>"+data["description"]+"</div>";
-	html+="<div class='date'>"+data["year"]+"</div>";
-	html+="</div>";
-	return html;
-}
+        <div id="movie_list" class="grid">
+        </div>
+    </body>
+</html>
